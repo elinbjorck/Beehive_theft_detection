2aa38018c84ee1c148ca0b28e43e87868414c009
Elin Björck <eb222wv@student.lnu.se>
2023-05-18 14:50:13 +0200

    All the work done before creating repo

---
+++ bee-theft/main.py
@@ -0,0 +1,120 @@
+import pycom
+from network import Bluetooth
+import machine
+import time
+
+nr_of_hives = 2
+id_set = set()
+id_set_counter = set()
+
+wait_time = 60
+adv_time = 3
+scan_time = 5
+
+hive_contact = False
+no_contact_count = 0
+max_no_contact = 5
+
+pycom.heartbeat(False)
+bluetooth = Bluetooth()
+pycom.rgbled(0x550000)  # Red
+
+def connection_callback (bt_o):
+    events = bt_o.events()   # this method returns the flags and clears the internal registry
+
+    if events & Bluetooth.CLIENT_CONNECTED:
+        pycom.rgbled(0x005500)  # green
+
+    elif events & Bluetooth.CLIENT_DISCONNECTED:
+        pycom.rgbled(0x550000)  # Red
+
+
+bluetooth.callback(trigger=Bluetooth.CLIENT_CONNECTED | Bluetooth.CLIENT_DISCONNECTED, handler=connection_callback)
+bluetooth.start_scan(-1)
+
+while len(id_set) < nr_of_hives:
+    adv = bluetooth.get_adv()
+
+    if adv:
+
+        if bluetooth.resolve_adv_data(adv.data, bluetooth.ADV_NAME_CMPL) == 'bee_hive':
+            id_set.add(adv.mac)
+    else:
+        time.sleep(0.5)
+
+for mac in id_set:
+    connection = None
+
+    try:
+        connection = bluetooth.connect(mac)
+        hive_contact = True
+
+    except:
+        print('Error while connecting to the bluetooth device or reading data.')
+    if connection:
+        connection.disconnect()
+
+print('Located correct number of hives. Saved mac-adresses.')    
+bluetooth.stop_scan()
+
+while True:
+
+    if hive_contact:
+        pycom.rgbled(0x000055)
+        bluetooth.set_advertisement(name = "guard_bee", manufacturer_data = "fipy", service_data = wait_time.to_bytes(4, 'big'))
+
+        bluetooth.advertise(True)
+        time.sleep(adv_time)
+        bluetooth.advertise(False)
+
+        pycom.rgbled(0x000000) #off
+        machine.sleep((wait_time - adv_time) * 1000)
+        pycom.rgbled(0x550000) #red
+
+        bluetooth = Bluetooth()
+        bluetooth.callback(trigger=Bluetooth.CLIENT_CONNECTED | Bluetooth.CLIENT_DISCONNECTED, handler=connection_callback)
+        bluetooth.start_scan(scan_time)
+        hive_contact = False
+
+    elif bluetooth.isscanning():
+        adv = bluetooth.get_adv()
+
+        if adv:
+
+            if bluetooth.resolve_adv_data(adv.data, bluetooth.ADV_NAME_CMPL) == 'bee_hive':
+                id_set.add(adv.mac)
+
+                if len(id_set) == nr_of_hives:
+                    connection = None
+                    try:
+                        connection = bluetooth.connect(adv.mac)
+                        id_set_counter.add(adv.mac)
+
+                    except:
+                        print('Error while connecting to the bluetooth device or reading data.')
+                    if connection:
+                        connection.disconnect()
+                        if not bluetooth.isscanning():
+                            bluetooth.start_scan(scan_time)
+                else:
+                    id_set.remove(adv.mac)
+                    print('Someone might be pretending to be a hive')
+
+        if len(id_set_counter) == len(id_set):
+            id_set_counter = set() 
+            no_contact_count = 0
+            print('All hives are good!')
+            hive_contact = True
+            bluetooth.stop_scan()
+    
+    else:
+        no_contact_count += 1
+
+        if no_contact_count < max_no_contact:
+            print('Could not find all hives. Trying again...')
+            bluetooth.start_scan(scan_time)
+
+        else:
+            print('some hives are missing')
+            no_contact_count = 0
+            hive_contact = True
---
+++ hive/main.py
@@ -0,0 +1,65 @@
+import pycom
+from network import Bluetooth
+import time
+import machine
+
+pycom.heartbeat(False)
+pycom.rgbled(0x550000)  # Red
+
+wait_time = None
+guard_contact = False
+advertising = False
+
+bluetooth = Bluetooth()
+
+def connection_callback (bt_o):
+    global guard_contact
+    events = bt_o.events()   # this method returns the flags and clears the internal registry
+    if events & Bluetooth.CLIENT_CONNECTED:
+        pycom.rgbled(0x005500)  # green
+    elif events & Bluetooth.CLIENT_DISCONNECTED:
+        guard_contact = True
+        pycom.rgbled(0x550000)  # Red
+
+
+# alive_message = bluetooth.service(b'0000000000000000')
+# alive_message.characteristic(uuid = '0000000000000001', value = b'Im OK')
+while True:
+
+    if guard_contact:
+        print('guard contact')
+        bluetooth.advertise(False)
+        advertising = False
+        bluetooth.start_scan(5)
+        pycom.rgbled(0x550000) #red
+        guard_contact = False
+
+    elif bluetooth.isscanning():
+        print('scanning after timing from guard')
+        adv = bluetooth.get_adv()
+
+        if adv:
+            print(bluetooth.resolve_adv_data(adv.data, bluetooth.ADV_NAME_CMPL))
+
+            if bluetooth.resolve_adv_data(adv.data, bluetooth.ADV_NAME_CMPL) == 'guard_bee':
+                print('found it')
+                wait_time = int.from_bytes(bluetooth.resolve_adv_data(adv.data, bluetooth.ADV_SERVICE_DATA), 'big')
+                bluetooth.stop_scan()
+
+    elif wait_time:
+        print('going to sleep')
+        pycom.rgbled(0x000000) #off
+        machine.sleep(wait_time * 1000)
+        print('woke up!')
+        wait_time = None
+
+    elif not advertising:
+        bluetooth = Bluetooth()
+        bluetooth.callback(trigger=Bluetooth.CLIENT_CONNECTED | Bluetooth.CLIENT_DISCONNECTED, handler=connection_callback)
+        
+        print('advertising!')
+        pycom.rgbled(0x000055) #blue
+
+        bluetooth.set_advertisement(name = "bee_hive", manufacturer_data = "fipy")
+        bluetooth.advertise(True)
+        advertising = True
